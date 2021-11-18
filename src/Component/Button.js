291d7ed6156439d090a614c0b1cf752e3ade3736
VaxobjanovDev <vaxobjanovdev@gmail.com>
2021-11-18 23:29:08 +0500

    Updated

--- src/Component/Button.js
+++ src/Component/Button.js
@@ -1,30 +1,56 @@
 import React from "react";
 import Product from "./Product";
 
-const Button = ({loading, response,setFilter, filter}) => {
-	const filterProduct = (category)=>{
-		const updatedList = response.filter((item)=>item.category===category)
-		setFilter(updatedList)
-	}
+const Button = ({ loading, response, setFilter, filter }) => {
+  const filterProduct = (category) => {
+    const updatedList = response.filter((item) => item.category === category);
+    setFilter(updatedList);
+  };
   return (
     <>
       <div className="row text-center">
         {loading ? (
-          "Loading"
-        ) : (<>
-          <div className="col-md-12 mt-5  mb-2 pb-5">
-            <button onClick={()=>setFilter(response)} className="btn btn-outline-dark my-2 me-2">All</button>
-            <button onClick={()=>filterProduct("men's clothing")} className="btn btn-outline-dark my-2 me-2">
-              Men's Clothing
-            </button>
-            <button onClick={()=>filterProduct("women's clothing")} className="btn btn-outline-dark my-2 me-2">
-              Women's Clothing
-            </button>
-            <button onClick={()=>filterProduct("jewelery")}className="btn btn-outline-dark my-2 me-2">Jewelry</button>
-            <button onClick={()=>filterProduct("electronics")} className="btn btn-outline-dark my-2 me-2">Electronics</button>
+          <div className="spinner text-center">
+            <div class="text-center spinner-border" role="status">
+              <span class="visually-hidden">Loading...</span>
+            </div>
           </div>
-					<Product filter={filter}/>
-					</>
+        ) : (
+          <>
+            <div className="col-md-12 mt-5  mb-2 pb-5">
+              <button
+                onClick={() => setFilter(response)}
+                className="btn btn-outline-dark my-2 me-2"
+              >
+                All
+              </button>
+              <button
+                onClick={() => filterProduct("men's clothing")}
+                className="btn btn-outline-dark my-2 me-2"
+              >
+                Men's Clothing
+              </button>
+              <button
+                onClick={() => filterProduct("women's clothing")}
+                className="btn btn-outline-dark my-2 me-2"
+              >
+                Women's Clothing
+              </button>
+              <button
+                onClick={() => filterProduct("jewelery")}
+                className="btn btn-outline-dark my-2 me-2"
+              >
+                Jewelry
+              </button>
+              <button
+                onClick={() => filterProduct("electronics")}
+                className="btn btn-outline-dark my-2 me-2"
+              >
+                Electronics
+              </button>
+            </div>
+            <Product filter={filter} />
+          </>
         )}
       </div>
     </>
